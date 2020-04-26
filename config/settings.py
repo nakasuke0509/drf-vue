bc7d04b3bc8ab2ae44fad2ad6571bbc0152e6a97
nakasuke <nakasuke0509.biz@gmail.com>
2020-04-26 16:27:08 +0900

    settings.pyのREST_Frameworkを修正

--- config/settings.py
+++ config/settings.py
@@ -133,6 +133,6 @@ STATIC_URL = '/static/'
 # REST Framework
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
 }
